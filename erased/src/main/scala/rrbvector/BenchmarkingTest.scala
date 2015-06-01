1f33b587662c659dafc84000ce872ac4075e73bb
milos <milos.stojanovic@epfl.ch>
2015-06-01 16:44:20 +0200

    rrb-vector benchmarks

---
+++ erased/src/main/scala/rrbvector/BenchmarkingTest.scala
@@ -0,0 +1,29 @@
+package rrbvector
+
+import scalameter._
+
+object BenchmarkingTest extends ScalameterBenchTest
+                           with GenericBenchTest
+                           with Serializable {
+
+  // the number of independent samples to use
+  lazy val sampleCount = 5
+  
+  override lazy val benchRunsPerSample = 1
+
+  // the command used to start the JVM
+  // HotSpot:
+  lazy val javaCommand = "java -server"
+  lazy val javaPreJDK7 = false
+
+  // the test size
+  lazy val testSizes = {
+    List(5000000)
+  }
+
+  withTestSize(5000000){
+
+    // run the tests:
+    testGeneric()
+  }
+}
---
+++ erased/src/main/scala/scalameter/BaseTest.scala
@@ -0,0 +1,7 @@
+package scalameter
+
+trait BaseTest {
+  def testSize: Int
+  def withTestSize(size: Int)(f : => Unit)
+  def test[T](transformation: String, tag: String, setup: Int => Unit, benchmark: => Unit, teardown: => Unit, extraJVMFlags: List[String] = Nil)
+}
